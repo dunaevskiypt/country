6cfaeabc95e552cb350bc5d906535a6305bab2bb
Peter <dunaevskiyp@gmail.com>
2023-04-26 15:35:29 +0300

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from './Container'
+import styled from "styled-components";   
+
+
+function Header() {
+  return (
+    <div>
+      <
+    </div>
+  )
+}
+
+export default Header
