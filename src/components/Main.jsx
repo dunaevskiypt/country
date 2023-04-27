a8ecd7fdcc5b6d3578be06bf31186e67f13a0e98
Peter <dunaevskiyp@gmail.com>
2023-04-27 08:34:40 +0300

    main comp dev

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+const Wrapper = styled.main ``
+import { Container } from "./Header/Container";
+const Main = () => {
+  return <div></div>;
+};
+
+export default Main;
